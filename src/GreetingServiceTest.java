5e7af4d0a71915d7934d7f3cc7ab0f0c0d77be77
Inas Fawzi <inasfawzi24@gmail.com>
2024-01-26 15:24:43 -0500

    added the names of both lab partners to the test file

--- src/GreetingServiceTest.java
+++ src/GreetingServiceTest.java
@@ -1,7 +1,7 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+//Lab 1, SOEN 345: Inas Fawzi 40208675 and Suha Abubakr 40120785
 public class GreetingServiceTest {
     GreetingService g = new GreetingService();
 
